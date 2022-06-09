cfd3e5947eff01ca579b07636750bcdf965caf57
pradeep swamireddy <pradeepntg@live.com>
2022-06-09 19:16:48 +0100

    validator fixes

--- src/main/java/com/mytelecom/repository/entity/MyTelecomUser.java
+++ src/main/java/com/mytelecom/repository/entity/MyTelecomUser.java
@@ -1,29 +1,34 @@
 package com.mytelecom.repository.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
+import lombok.Data;
 
 @Data
 @Entity
 public class MyTelecomUser {
-   @NotNull
-   @Id
-   private String id;
-   @Size(min=8)
-   private String password;
-   private String name;
-   private String phoneNumber;
-   @JsonDeserialize(using = LocalDateDeserializer.class)
-   @JsonSerialize(using = LocalDateSerializer.class)
-   private LocalDate dateOfBirth;
-   private String billingAddress;
+	@NotBlank
+	@Id
+	private String id;
+	@Size(min = 8)
+	private String password;
+	@NotBlank
+	private String name;
+	@NotBlank
+	private String phoneNumber;
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	@JsonSerialize(using = LocalDateSerializer.class)
+	private LocalDate dateOfBirth;
+	@NotBlank
+	private String billingAddress;
 }
